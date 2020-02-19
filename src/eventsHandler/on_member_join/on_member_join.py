36f323ce6103e08943fe928c04248ec7c73bcbbb
Baptiste Bourdet <baptiste.bourdet@epita.fr>
2020-02-19 22:51:24 +0100

    fix(on member join): fix weird \n in welcome msg

--- src/eventsHandler/on_member_join/on_member_join.py
+++ src/eventsHandler/on_member_join/on_member_join.py
@@ -5,14 +5,14 @@ from src.utils.embeds_manager import EmbedsManager
 
 class OnMemberJoin:
 
-    msg_fr: str = """Bienvenue sur le serveur {}.\n\n:red_circle: Pour avoir accès au serveur, vous devez 
-    prouver que vous êtes un étudiant de l'école. Pour cela, merci de vous manifester dans le channel #contacts_staff. 
-    Un modérateur s'occupera de vous valider et de vous donner les bons rôles.\n\n:grey_question: Vous pouvez 
+    msg_fr: str = """Bienvenue sur le serveur {}.\n\n:red_circle: Pour avoir accès au serveur, vous devez \
+    prouver que vous êtes un étudiant de l'école. Pour cela, merci de vous manifester dans le channel #contacts_staff. \
+    Un modérateur s'occupera de vous valider et de vous donner les bons rôles.\n\n:grey_question: Vous pouvez \
     mentionner @Modo si le staff ne remarque pas votre arrivée."""
 
-    msg_eng: str = """Welcome to the {} server.\n\n:red_circle: To get access to the server, you need 
-    to prove that you are a student of the school. For this, please send a message in the #contacts_staff channel.
-     A moderator will take care of validating your status and giving you the correct roles.\n\n:grey_question: You 
+    msg_eng: str = """Welcome to the {} server.\n\n:red_circle: To get access to the server, you need \
+    to prove that you are a student of the school. For this, please send a message in the #contacts_staff channel.\
+     A moderator will take care of validating your status and giving you the correct roles.\n\n:grey_question: You \
      can mention @Modo if the staff does not notice your arrival."""
 
     @staticmethod
